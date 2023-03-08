03d0a017a972fd1e4e7514b33725f2c59f4b6366
MinhQuan-C4EJS134 <minhquan90.ksxd@gmail.com>
2023-03-08 16:13:16 +0700

    ok

---
+++ checkbox/src/App.js
@@ -0,0 +1,20 @@
+
+import  ReactDOM  from 'react-dom/client'
+import {useState} from 'react'
+import Checkbox from './check/check'
+
+
+
+function App() {
+  
+  return(
+      <>
+      <div>
+          <Checkbox/>
+      </div>
+      
+
+      </>
+  )
+}
+export default App;
